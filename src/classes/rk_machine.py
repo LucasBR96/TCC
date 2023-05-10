af6ee088c17c254e2affdc966d4e2e4b3092ba5c
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2023-05-10 13:46:02 -0300

    rk machine updated, not it can be of any order

--- src/classes/rk_machine.py
+++ src/classes/rk_machine.py
@@ -1,19 +1,20 @@
 import numpy as np
 from typing import *
 
-class rk2_machine:
+class rk_machine:
 
     '''
-    runge kutta of degree two machine
+    runge kutta of any order machine
     '''
 
-    def __init__( self , X : np.ndarray , h : float, der_fun : Callable , h_fun : Callable ):
+    def __init__( self , X : np.ndarray , h : float, der_fun : Callable , rk_fun : Callable , h_fun : Callable = None ):
 
         self.X = X              # Initial conditions of the system
         self.X_dot = None       # Derivation of the system
         self.h = h              # Current time resolution
         self.der_fun = der_fun  # X update function
         self.h_fun = h_fun      # Adaptative timestep function
+        self.rk_fun = rk_fun    # Runge kutta function
 
         self.time_elapsed = 0   # Time since the simulation started
 
@@ -23,11 +24,7 @@ class rk2_machine:
             
             #-----------------------------------------------------
             # Updating the state, using the range_kutta 2 method
-            k1 = self.der_fun( self.X )
-            k2 = self.der_fun(
-                self.X + self.h*k1 
-            )
-            X_dot = ( k1 + k2 )/2
+            X_dot = self.rk_fun( self.X , self.h , self.der_fun )
             self.X = self.X + self.h*X_dot
 
             #---------------------------------------------
@@ -39,3 +36,26 @@ class rk2_machine:
         tup = ( self.time_elapsed , self.X.copy() )
         self.time_elapsed += self.h
         return tup
+
+def rk2( X : np.ndarray , h : float, der_fun : Callable ):
+
+    k1 = der_fun( X )
+    k2 = der_fun(
+        X + h*k1 
+    )
+    return ( k1 + k2 )/2
+
+def get_rk2_machine( X : np.ndarray , h : float, der_fun : Callable , h_fun : Callable = None ):
+    return rk_machine( X , h , der_fun , rk2 , h_fun )
+
+def rk4( X : np.ndarray , h : float, der_fun : Callable ):
+
+    k1 = der_fun( X )
+    k2 = der_fun( X + k1*( h/2 ) )
+    k3 = der_fun( X + k2*( h/2 ) )
+    k4 = der_fun( X + k3*h )
+
+    return ( k1 + 2*k2 + 2*k3 + k4 )/6
+
+def get_rk4_machine( X : np.ndarray , h : float, der_fun : Callable , h_fun : Callable = None ):
+    return rk_machine( X , h , der_fun , rk4 , h_fun )
